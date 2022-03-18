bc142a06ab23a20f77e0782d657f4bf34d257242
Diego Salvio <diantonio46@gmail.com>
2022-03-18 14:37:35 -0300

    Finalizamos a apostila 02

---
+++ aula.js
@@ -0,0 +1,44 @@
+//axios
+
+function hello(nome){
+    return 
+}
+
+/*
+require('dotenv').config()
+const axios = require('axios')
+
+const {PROTOCOL, BASE_URL, APPID, Q, CNT, UNITS, LANGUAGE} = process.env
+
+
+const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&cnt=${CNT}&units=${UNITS}&lang=${LANGUAGE}`
+
+console.log(url)
+
+axios.get(url)
+.then(res => {
+    //console.log(res)
+    return res.data
+})
+.then(res => {
+    console.log(res)
+    return res
+})
+.then(res => {
+    console.log(res.cnt)
+    return res
+})
+.then(res => {
+    return res.list
+})
+.then(res=> {
+    res.forEach(element => {
+        console.log(element.main.temp_min)
+        console.log("\n")
+        console.log(element.main.temp_max)
+        console.log("\n")
+        console.log(element.weather[0].description)
+    });
+})
+
+*/
